20ef92ac2123b22c0e6c9506a9e2c15fac77706d
Heedong <59307414+ruthetum@users.noreply.github.com>
2024-09-14 16:25:48 +0900

    프로젝트 셋업

---
+++ src/main/kotlin/labs/wilump/hierarchical/HierarchicalApplication.kt
@@ -0,0 +1,11 @@
+package labs.wilump.hierarchical
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HierarchicalApplication
+
+fun main(args: Array<String>) {
+	runApplication<HierarchicalApplication>(*args)
+}
